43c2afba246207d2aae34380418d909df43ca6d0
Suren Nihalani <1093911+SurenNihalani@users.noreply.github.com>
2024-11-10 11:24:52 -0800

    Add WAF

--- terraform/modules/stables_environment/virtual-network.tf
+++ terraform/modules/stables_environment/virtual-network.tf
@@ -44,3 +44,36 @@ resource "azurerm_subnet" "desktop_subnet" {
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main_network.name
 }
+
+resource "azurerm_subnet" "firewall_subnet" {
+  name = "AzureFirewallSubnet"
+  address_prefixes = [
+    cidrsubnet(var.ip_network, 8, 3),
+  ]
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main_network.name
+}
+
+
+resource "azurerm_public_ip" "main_waf" {
+  name                = "main_waf"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+
+resource "azurerm_firewall" "waf" {
+  name                = "firewall"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku_name            = "AZFW_VNet"
+  sku_tier            = "Standard"
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.firewall_subnet.id
+    public_ip_address_id = azurerm_public_ip.main_waf.id
+  }
+}
